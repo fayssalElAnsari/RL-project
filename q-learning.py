4fb0f04e21cffa88ac3b1941f2951cf8d39da526
Fayssal EL ANSARI <fayssal.el.ansari@gmail.com>
2023-11-03 14:23:00 +0100

    Update q-learning.py

--- q-learning.py
+++ q-learning.py
@@ -21,8 +21,23 @@ lr = args.lr
 gamma = args.gamma
 num_episodes = args.num_episodes
 
+negative_reward_enabled = False
+is_slippery_enabled = True
+
+# early stopping
+average_reward_threshold = 0.75
+consecutive_episodes = 100  # Number of episodes to consider for moving average
+moving_average_rewards = []
+
+custom_map = [
+    'SFFF',
+    'FHFF',
+    'FFHF',
+    'HFGF'
+]
+
 # Create the environment
-env = gym.make('FrozenLake-v1', is_slippery=True)
+env = gym.make('FrozenLake-v1', desc=custom_map, is_slippery=is_slippery_enabled)
 
 # Initialize the Q-table
 Q = np.zeros([env.observation_space.n, env.action_space.n])
@@ -32,11 +47,6 @@ total_rewards = []
 total_steps = []
 success_rate = []
 test_reward = []
-
-# early stopping
-average_reward_threshold = 0.2
-consecutive_episodes = 200  # Number of episodes to consider for moving average
-moving_average_rewards = []
 
 start_time = time.time()
 
@@ -51,6 +61,10 @@ for i in range(num_episodes):
     while not done:
         a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
         s_, r, done, _ = env.step(a)
+
+        if negative_reward_enabled:
+            if done and r == 0:  # The agent fell into a hole and not at the goal state
+                r = -1  # Negative reward for falling into a hole
         
         # update Q values
         Q[s,a] = Q[s,a] + lr*(r + gamma*np.max(Q[s_,:]) - Q[s,a])
@@ -126,9 +140,6 @@ def play_game(env, Q):
 
 # Call the play_game function to visualize the agent playing
 play_game(env, Q)
-
-
-
 
 # Plotting the averge rewards and episode Lengths gained throughout each episode per episode
 fig, axs = plt.subplots(1, 2, figsize=(200, 5))
